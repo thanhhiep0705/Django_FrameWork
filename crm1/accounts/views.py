8a58fa0821ffd3f2d6c2ff90ab7daabb976b78d0
thanhhiep0705 <thanhhiep0705@gmail.com>
2022-04-22 10:38:31 +0700

    P12-Inline Formsets

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 # Create your views here.
 from .models import *
 from .forms import OrderForm
@@ -36,23 +37,22 @@ def customer(request, pk_test):
 	return render(request, 'accounts/customer.html',context)
 
 
-def createOrder(request):
-	form = OrderForm()
+def createOrder(request, pk):
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5 )
+	customer = Customer.objects.get(id = pk)
+	formset = OrderFormSet(queryset = Order.objects.none(),instance= customer)
 	if request.method == 'POST':
 		#print('Printing POST:', request.POST)
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
+		formset = OrderFormSet(request.POST, instance= customer)
+		if formset.is_valid():
+			formset.save()
 			return redirect('/')
-
-	context = {'form':form}
+	context = {'formset':formset}
 	return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
-
 	order = Order.objects.get(id=pk)
 	form = OrderForm(instance=order)
-
 	if request.method == 'POST':
 		form = OrderForm(request.POST, instance=order)
 		if form.is_valid():
